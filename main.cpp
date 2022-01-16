2fb86501c2f0d3784b08b744b7ab2e2133026247
DimasFadilla06 <97537821+DimasFadilla06@users.noreply.github.com>
2022-01-16 17:53:40 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //looping
+    int a = 1;
+
+   while( a < 20 ) {
+      cout << "nilai dari a: "<< a <<endl;
+      a++;
+   }
+   cout<<"\n selesai \n"<<endl;
+    return 0;
+}
